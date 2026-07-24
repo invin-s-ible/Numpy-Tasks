d078a94f97742a7fa8dc5b0a0f80884c6755cdf3
invin-s-ible <sangalasaikumar05@gmail.com>
2026-07-24 22:15:35 +0530

    Create Day3-practicearray.py

---
+++ Day3-practicearray.py
@@ -0,0 +1,14 @@
+squares = np.arange(1, 11) ** 2
+print(squares)
+data = np.array([12, 45, 7, 23, 56, 89, 34])
+print("Sum:", data.sum())
+print("Mean:", data.mean())
+print("Max:", data.max())
+arr = np.arange(12)
+reshaped = arr.reshape(3, 4)
+print(reshaped)
+filtered = data[data > 20]
+print(filtered)
+a = np.array([1, 5, 3])
+b = np.array([2, 4, 3])
+print(a > b)
